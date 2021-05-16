0bf3471323063a2c1e17dfeec45a72f6885b9248
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2021-05-16 17:15:38 +0530

    Add files via upload

---
+++ Chef and SnackDown.py
@@ -0,0 +1,7 @@
+T = int(input())
+for i in range(T):
+    N = int(input())
+    if N == 2010 or N == 2015 or N == 2016 or N == 2017 or N == 2019:
+        print("HOSTED")
+    else:
+        print("NOT HOSTED")
